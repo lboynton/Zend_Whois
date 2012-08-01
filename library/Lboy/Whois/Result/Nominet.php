fa0ddbd923f8b9e3054c6a01493e15ae8fa6a57b
Lee Boynton <lee@lboynton.com>
2012-08-01 09:22:20 +0100

    Start on getting nameservers from Nominet

--- library/Lboy/Whois/Result/Nominet.php
+++ library/Lboy/Whois/Result/Nominet.php
@@ -12,4 +12,34 @@ class Lboy_Whois_Result_Nominet extends Lboy_Whois_Result_AbstractResult
 		$start = strpos($this->whois, "Expiry date:");
 		return substr($this->whois, $start + 14, 11);
 	}
+	
+	public function getNameServers()
+	{
+		$lines = explode(PHP_EOL, $this->whois);
+		$found = false;
+		$servers = array();
+		
+		foreach($lines as $i => $line)
+		{
+			$line = trim($line);
+			
+			if (strpos($line, "Name servers:") !== false)
+			{
+				$found = true;
+				continue;
+			}
+			
+			if ($found)
+			{
+				if (empty($line))
+				{
+					break;
+				}
+			
+				$servers[] = $line;
+			}
+		}
+		
+		return $servers;
+	}
 }
--- tests/library/Lboy/Whois/Result/NominetTest.php
+++ tests/library/Lboy/Whois/Result/NominetTest.php
@@ -17,12 +17,48 @@ class Lboy_Whois_Result_NominetTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals($expiry, $result->getExpiry());
 	}
 	
+	/**
+	 * @dataProvider getNameServers
+	 */
+	public function testGetNameServers($domain, $nameServers)
+	{
+		$whois = file_get_contents(dirname(__FILE__) . '/resources/' . $domain); 
+		$result = new Lboy_Whois_Result_Nominet($whois);
+		$this->assertEquals($nameServers, $result->getNameServers());
+	}
+	
 	public function getWhois()
 	{
 		return array
 		(
 			array('amazon.co.uk', '05-Dec-2012'),
 			array('bbc.co.uk', '13-Dec-2012')
+		);
+	}
+	
+	public function getNameServers()
+	{
+		return array
+		(
+			array
+			(
+				'amazon.co.uk', array
+				(
+					'ns1.p31.dynect.net', 'ns2.p31.dynect.net', 
+					'ns3.p31.dynect.net', 'ns4.p31.dynect.net', 
+					'pdns1.ultradns.net','pdns2.ultradns.net', 
+					'pdns3.ultradns.org', 'pdns4.ultradns.org', 
+					'pdns5.ultradns.info', 'pdns6.ultradns.co.uk'
+				)
+			),
+			array
+			(	
+				'bbc.co.uk', array
+				(
+					'ns1.rbsov.bbc.co.uk', 'ns1.tcams.bbc.co.uk', 
+					'ns1.thdow.bbc.co.uk'
+				)
+			)
 		);
 	}
 }
