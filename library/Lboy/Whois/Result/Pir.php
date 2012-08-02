4cef9fd1dcd835f3b140f45bb0a49e4b24cca98f
Lee Boynton <lee@lboynton.com>
2012-08-02 08:40:39 +0100

    Add method to get nameservers from pir whois

--- library/Lboy/Whois/Result/Pir.php
+++ library/Lboy/Whois/Result/Pir.php
@@ -12,4 +12,25 @@ class Lboy_Whois_Result_Pir extends Lboy_Whois_Result_AbstractResult
 		$start = strpos($this->whois, "Expiration Date:");
 		return substr($this->whois, $start + 16, 11);
 	}
+	
+	public function getNameServers()
+	{
+		$lines = explode(PHP_EOL, $this->whois);
+		$matching = array();
+		
+		foreach($lines as $line)
+		{
+			if (strpos($line, "Name Server:") !== false)
+			{
+				$server = substr(trim($line), 12);
+				
+				if ($server)
+				{
+					$matching[] = $server;
+				}
+			}
+		}
+		
+		return $matching;
+	}
 }
--- tests/library/Lboy/Whois/Result/PirTest.php
+++ tests/library/Lboy/Whois/Result/PirTest.php
@@ -24,4 +24,29 @@ class Lboy_Whois_Result_PirTest extends PHPUnit_Framework_TestCase
 			array('pir.org', '19-Feb-2015')
 		);
 	}
+	
+	/**
+	 * @dataProvider getNameServers
+	 */
+	public function testGetNameServers($domain, $nameServers)
+	{
+		$whois = file_get_contents(dirname(__FILE__) . '/resources/' . $domain); 
+		$result = new Lboy_Whois_Result_Pir($whois);
+		$this->assertEquals($nameServers, $result->getNameServers());
+	}
+	
+	public function getNameServers()
+	{
+		return array
+		(
+			array
+			(
+				'pir.org', array
+				(
+					'NS1.AMS1.AFILIAS-NST.INFO', 'NS1.MIA1.AFILIAS-NST.INFO', 
+					'NS1.SEA1.AFILIAS-NST.INFO', 'NS1.YYZ1.AFILIAS-NST.INFO'
+				)
+			)
+		);
+	}
 }
