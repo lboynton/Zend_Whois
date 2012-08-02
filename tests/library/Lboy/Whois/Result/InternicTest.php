2a219d62c078913e820d0711b9d800ac411ce768
Lee Boynton <lee@lboynton.com>
2012-08-02 09:09:15 +0100

    Add some .net domains to test

--- tests/library/Lboy/Whois/Result/InternicTest.php
+++ tests/library/Lboy/Whois/Result/InternicTest.php
@@ -34,7 +34,10 @@ class Lboy_Whois_Result_InternicTest extends PHPUnit_Framework_TestCase
 			array('amazon.com', '31-oct-2021'),
 			array('apple.com', '20-feb-2020'),
 			array('google.com', '14-sep-2020'),
-			array('twitter.com', '21-jan-2019')
+			array('twitter.com', '21-jan-2019'),
+			array('arin.net', '18-dec-2021'),
+			array('php.net', '17-nov-2012'),
+			array('sourceforge.net', '08-aug-2013')
 		);
 	}
 	
@@ -54,11 +57,51 @@ class Lboy_Whois_Result_InternicTest extends PHPUnit_Framework_TestCase
 				)
 			),
 			array
+			(
+				'apple.com', array
+				(
+					'NSERVER.APPLE.COM', 'NSERVER2.APPLE.COM', 
+					'NSERVER3.APPLE.COM', 'NSERVER4.APPLE.COM', 
+					'NSERVER5.APPLE.COM', 'NSERVER6.APPLE.COM'
+				)
+			),
+			array
+			(
+				'google.com', array
+				(
+					'NS1.GOOGLE.COM', 'NS2.GOOGLE.COM', 'NS3.GOOGLE.COM', 
+					'NS4.GOOGLE.COM'
+				)
+			),
+			array
 			(	
 				'twitter.com', array
 				(
 					'NS1.P34.DYNECT.NET', 'NS2.P34.DYNECT.NET', 
 					'NS3.P34.DYNECT.NET', 'NS4.P34.DYNECT.NET'
+				)
+			),
+			array
+			(
+				'arin.net', array
+				(
+					'NS1.ARIN.NET', 'NS2.ARIN.NET', 'U.ARIN.NET', 'V.ARIN.NET'
+				)
+			),
+			array
+			(
+				'php.net', array
+				(
+					'DNS1.EASYDNS.COM', 'DNS2.EASYDNS.NET', 'DNS3.EASYDNS.ORG', 
+					'DNS4.EASYDNS.INFO'
+				)
+			),
+			array
+			(
+				'sourceforge.net', array
+				(
+					'NS1.P03.DYNECT.NET', 'NS2.P03.DYNECT.NET', 
+					'NS3.P03.DYNECT.NET', 'NS4.P03.DYNECT.NET'
 				)
 			)
 		);
