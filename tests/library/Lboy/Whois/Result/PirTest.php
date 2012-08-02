174a87b2e877a121acda127f6c7bf2316d29b4b2
Lee Boynton <lee@lboynton.com>
2012-08-02 09:03:33 +0100

    Add some more .org domains to test

--- tests/library/Lboy/Whois/Result/PirTest.php
+++ tests/library/Lboy/Whois/Result/PirTest.php
@@ -21,7 +21,9 @@ class Lboy_Whois_Result_PirTest extends PHPUnit_Framework_TestCase
 	{
 		return array
 		(
-			array('pir.org', '19-Feb-2015')
+			array('pir.org', '19-Feb-2015'),
+			array('slashdot.org', '04-Oct-2012'),
+			array('wikipedia.org', '13-Jan-2016')
 		);
 	}
 	
@@ -45,7 +47,17 @@ class Lboy_Whois_Result_PirTest extends PHPUnit_Framework_TestCase
 				(
 					'NS1.AMS1.AFILIAS-NST.INFO', 'NS1.MIA1.AFILIAS-NST.INFO', 
 					'NS1.SEA1.AFILIAS-NST.INFO', 'NS1.YYZ1.AFILIAS-NST.INFO'
-				)
+				),
+				'slashdot.org', array
+				(
+					'NS3.P03.DYNECT.NET', 'NS1.P03.DYNECT.NET', 
+					'NS2.P03.DYNECT.NET', 'NS4.P03.DYNECT.NET'
+				),
+				'wikipedia.org', array
+				(
+					'NS0.WIKIMEDIA.ORG', 'NS1.WIKIMEDIA.ORG', 
+					'NS2.WIKIMEDIA.ORG'
+				),
 			)
 		);
 	}
