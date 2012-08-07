3239d40124282381d5d9c2ea0dfa172ad189a076
Lee Boynton <lee@lboynton.com>
2012-08-07 09:21:03 +0100

    Add test for .biz domains. To do next.

---
+++ library/Lboy/Whois/Result/Neustar.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @link http://github.com/lboynton/Zend_Whois for the canonical source repository
+ * @copyright (c) 2012, Lee Boynton
+ * @author Lee Boynton <lee@lboynton.com>
+ */
+class Lboy_Whois_Result_Neustar extends Lboy_Whois_Result_AbstractResult
+{
+	public function getExpiry()
+	{
+		
+	}
+
+	public function getNameServers()
+	{
+		
+	}
+
+	public function isRateLimitExceeded()
+	{
+		
+	}
+}
---
+++ tests/library/Lboy/Whois/Result/NeustarTest.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * @link http://github.com/lboynton/Zend_Whois for the canonical source repository
+ * @copyright (c) 2012, Lee Boynton
+ * @author Lee Boynton <lee@lboynton.com>
+ */
+class Lboy_Whois_Result_NeustarTest extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * @dataProvider getWhois
+	 */
+	public function testGetExpiryDate($domain, $expiry)
+	{
+		$whois = file_get_contents(dirname(__FILE__) . '/resources/' . $domain); 
+		$result = new Lboy_Whois_Result_Neustar($whois);
+		$this->assertEquals($expiry, $result->getExpiry());
+	}
+	
+	public function getWhois()
+	{
+		return array
+		(
+			array('neustar.biz', 'Tue Nov 06')
+		);
+	}
+	
+	/**
+	 * @dataProvider getNameServers
+	 */
+	public function testGetNameServers($domain, $nameServers)
+	{
+		$whois = file_get_contents(dirname(__FILE__) . '/resources/' . $domain); 
+		$result = new Lboy_Whois_Result_Neustar($whois);
+		$this->assertEquals($nameServers, $result->getNameServers());
+	}
+	
+	public function getNameServers()
+	{
+		return array
+		(
+			array
+			(
+				'neustar.biz', array
+				(
+					'PDNS1.ULTRADNS.NET', 'PDNS2.ULTRADNS.NET', 
+					'PDNS3.ULTRADNS.ORG', 'PDNS4.ULTRADNS.ORG',
+					'PDNS5.ULTRADNS.INFO', 'PDNS6.ULTRADNS.CO.UK'
+				)
+			)
+		);
+	}
+}
