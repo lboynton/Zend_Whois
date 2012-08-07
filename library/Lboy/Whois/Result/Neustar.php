8121b2630181a8e3514c5450afcab343f50cb761
Lee Boynton <lee@lboynton.com>
2012-08-07 22:37:59 +0100

    Implement Neustar whois parser

--- library/Lboy/Whois/Result/Neustar.php
+++ library/Lboy/Whois/Result/Neustar.php
@@ -9,16 +9,34 @@ class Lboy_Whois_Result_Neustar extends Lboy_Whois_Result_AbstractResult
 {
 	public function getExpiry()
 	{
-		
+		$start = strpos($this->whois, "Domain Expiration Date:");
+		return substr($this->whois, $start + 45, 10);
 	}
 
 	public function getNameServers()
 	{
+		$lines = explode(PHP_EOL, $this->whois);
+		$matching = array();
 		
+		foreach($lines as $line)
+		{
+			if (strpos($line, "Name Server:") !== false)
+			{
+				$server = trim(substr($line, 12));
+				
+				if ($server)
+				{
+					$matching[] = $server;
+				}
+			}
+		}
+		
+		return $matching;
 	}
 
 	public function isRateLimitExceeded()
 	{
-		
+		// TODO
+		return false;
 	}
 }
